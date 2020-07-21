2948b133a0bcddf5086fc23d4f01c39615e114c4
Martin Folkers <hello@twobrain.io>
2020-07-21 10:51:12 +0200

    Suiting our needs - Turning IDs into classes - Removing inline CSS - Removing gallery counter - Removing caption button - Removing close button - Adding custom buttons - Adding caption zig-zag border

--- src/BigPicture.js
+++ src/BigPicture.js
@@ -30,9 +30,6 @@ let iframeSiteVid
 // store requested image source
 let imgSrc
 
-// button that closes the container
-let closeButton
-
 // youtube / vimeo video id
 let siteVidID
 
@@ -48,14 +45,14 @@ let loadingIcon
 // caption element
 let caption
 
+// store caption border
+let captionBorder
+
 // caption content element
 let captionText
 
 // store caption content
 let captionContent
-
-// hide caption button element
-let captionHideButton
 
 // open state for container element
 let isOpen
@@ -89,9 +86,6 @@ let galleryPosition
 
 // hold active gallery els / image src
 let galleryEls
-
-// counter element
-let galleryCounter
 
 // store images in gallery that are being loaded
 let preloadedImages = {}
@@ -189,40 +183,37 @@ export default (options) => {
 // create all needed methods / store dom elements on first use
 function initialize() {
 	let startX
-	// return close button elements
-	function createCloseButton(className) {
-		const el = document[createEl]('button')
-		el.className = className
-		el.innerHTML =
-			'<svg viewBox="0 0 48 48"><path d="M28 24L47 5a3 3 0 1 0-4-4L24 20 5 1a3 3 0 1 0-4 4l19 19L1 43a3 3 0 1 0 4 4l19-19 19 19a3 3 0 0 0 4 0v-4L28 24z"/></svg>'
-		return el
-	}
 
-	function createArrowSymbol(direction, style) {
+	function createArrowSymbol(nextImage, direction) {
 		const el = document[createEl]('button')
-		el.className = 'bp-lr'
-		el.innerHTML =
-			'<svg viewBox="0 0 129 129" height="70" fill="#fff"><path d="M88.6 121.3c.8.8 1.8 1.2 2.9 1.2s2.1-.4 2.9-1.2a4.1 4.1 0 0 0 0-5.8l-51-51 51-51a4.1 4.1 0 0 0-5.8-5.8l-54 53.9a4.1 4.1 0 0 0 0 5.8l54 53.9z"/></svg>'
-		changeCSS(el, style)
+		const padding = direction === 'left'
+			? 'pr-20 py-24 lg:py-0'
+			: 'pl-20 py-24 lg:py-0'
+		el.className = 'bp-arrow ' + padding + ' ' + direction + '-0'
+
+		const arrow = document[createEl]('DIV')
+		const rounded = direction === 'left'
+			? 'rounded-r-lg'
+			: 'rounded-l-lg'
+
+		arrow.className = rounded + ' px-6 py-4 bg-red-light hover:bg-red-medium transition-all'
+		arrow.innerHTML = direction === 'left'
+			? '<svg class="w-auto h-12 text-white fill-current" viewBox="0 0 37 32"><path d="M34.904 15.396a30.8 30.8 0 00.275-2.893c.092-1.652-2.203-2.111-2.984-.826-.046.092-.092.138-.138.23-.275-.551-.826-.873-1.423-.919.092-.32.184-.688.322-1.01.505-1.606-1.837-2.754-2.755-1.514-.184-.184-.46-.322-.689-.414.092-.275.184-.505.276-.78.55-1.561-1.286-2.387-2.433-1.699l.137-.55c.368-1.7-2.112-2.939-2.938-1.24-.459.918-.872 1.79-1.331 2.708.137-1.01-.689-1.652-1.515-1.698.275-.964.55-1.883.826-2.8.55-1.791-2.02-2.755-2.938-1.24a49.63 49.63 0 00-2.617 4.866c-.046-1.148-1.331-1.79-2.25-1.423-.046-.46-.091-.964-.183-1.423-.276-1.378-2.296-1.515-2.938-.368a191.834 191.834 0 00-2.801 5.28c.092-.413.138-.826.23-1.285.32-1.653-1.424-2.571-2.663-1.561C2.813 6.077 1.527 7.499.38 9.107c-1.102 1.56 1.24 3.075 2.525 1.882-.551 2.846-1.056 5.692-1.607 8.493-.322 1.653 2.158 3.03 2.938 1.24.092-.184.138-.322.23-.506.459.505 1.147.78 1.698.551-.367 1.607-.734 3.214-1.147 4.867-.414 1.836 1.928 2.754 2.938 1.24.505-.735.964-1.47 1.423-2.204.413.275 1.056.367 1.607.23-.413 1.698-.78 3.35-1.194 5.05-.367 1.698 2.112 2.937 2.938 1.239.643-1.286 1.286-2.617 1.929-3.903.137 1.01 1.24 1.515 2.157 1.24-.091 1.79 2.48 2.387 3.122.55.643-1.835 1.332-3.672 1.974-5.508-.367 1.01-.734 2.02-1.102 3.076-.688 1.974 2.434 2.8 3.122.872.322-.872.643-1.745.964-2.663 0 .551.046 1.102.092 1.653.138 1.745 2.663 2.25 3.168.413.643-2.433 1.653-4.637 2.8-6.749-.367 2.158-.734 4.362-.826 6.566-.092 1.882 2.617 2.111 3.168.413.505-1.653 1.056-3.306 1.56-5.004.46-1.515.781-3.168 1.745-4.408.919-1.285-.413-2.525-1.698-2.341zm-4.316 1.423a1.81 1.81 0 00-.23.643c0 .413-.229.597-.642.643h-.138c-.23-.23-.55-.138-.826-.138-.643.046-1.286 0-1.882.046-.505.046-1.056 0-1.561.138-.23.046-.46.046-.689.046-1.01 0-1.974.183-2.984.092-1.01-.093-1.974 0-2.984 0 0 .045-.046.091-.046.091l.183.184c.276.23.505.46.597.826.092.368.276.735.368 1.148.045.184 0 .413 0 .597 0 .46-.184 1.01-.276 1.47-.046.229-.092.458-.23.642-.137.184-.413.321-.596.23a1.01 1.01 0 01-.23-.138c-.183-.092-.367-.092-.55-.138-.322-.092-.552-.275-.827-.46-.597-.458-1.331-.734-2.02-1.1-.413-.23-.78-.552-1.194-.827-.321-.23-.688-.413-1.056-.597-.413-.23-.872-.413-1.285-.643-.092-.046-.138-.138-.23-.23l-.321-.32c-.138-.368-.184-.827-.413-1.194-.092-.092-.092-.276-.138-.414-.092-.413-.138-.872-.23-1.285 0-.092 0-.184.046-.276.046-.23.092-.459-.091-.642.045-.184.091-.322.091-.46 0-.183.092-.367.276-.458l.275-.138c.643-.23.918-.872 1.378-1.286.459-.413.826-.872 1.423-1.056.092 0 .137-.092.23-.137.183-.092.32-.184.504-.322.092-.046.23-.092.276-.183.321-.368.734-.505 1.193-.643.23-.092.46-.275.69-.413.55-.322 1.01-.689 1.652-.827.046 0 .092-.046.138-.092.32-.183.55-.092.734.23.276.459.23 1.01.367 1.469.046.184.046.367.276.46.413.275.55.78.413 1.285-.184.596-.55 1.056-1.01 1.469l-.413.413c.688 0 1.331.046 1.928-.138.138-.046.321-.092.505-.092.413 0 .826-.046 1.194-.184.046 0 .092 0 .137-.045.368-.368.919-.414 1.378-.597.137-.046.23-.138.321-.184.551-.046 1.056-.092 1.607-.184.092 0 .184-.137.321-.137.23-.046.413-.184.643 0 .046.046.138 0 .184 0 .137 0 .321-.046.459-.046.642 0 1.24.137 1.882.23h.046c.413.412 1.102.55 1.194 1.285.046.321.23.642.321.964.046.137.046.275.046.413 0 .092-.046.184 0 .23.092.32-.092.55-.184.78z"/></svg>'
+			: '<svg class="w-auto h-12 text-white fill-current" viewBox="0 0 37 32"><path d="M2.096 16.604a30.8-30.8 0 00-.275 2.893c-.092 1.652 2.203 2.111 2.984.826.046-.092.092-.138.138-.23.275.551.826.873 1.423.92-.092.32-.184.687-.322 1.01-.505 1.605 1.837 2.753 2.755 1.513.184.184.46.322.69.414-.093.275-.185.505-.277.78-.55 1.561 1.286 2.387 2.433 1.7l-.137.55c-.368 1.7 2.112 2.938 2.938 1.24.46-.919.872-1.79 1.331-2.709-.137 1.01.69 1.652 1.515 1.698-.275.964-.55 1.883-.826 2.8-.55 1.791 2.02 2.755 2.938 1.24a49.63-49.63 0 002.617-4.866c.046 1.148 1.331 1.79 2.25 1.423.046.46.091.964.183 1.423.276 1.378 2.296 1.515 2.938.368a191.834-191.834 0 002.801-5.28c-.092.413-.138.826-.23 1.285-.32 1.653 1.424 2.571 2.663 1.561 1.561-1.24 2.847-2.662 3.994-4.27 1.102-1.56-1.24-3.075-2.525-1.882.551-2.846 1.056-5.692 1.607-8.493.322-1.653-2.158-3.03-2.938-1.24-.092.184-.138.322-.23.506-.459-.505-1.147-.78-1.698-.55.367-1.608.734-3.215 1.147-4.868.414-1.836-1.928-2.754-2.938-1.24-.505.735-.964 1.47-1.423 2.204-.413-.275-1.056-.367-1.607-.23.413-1.698.78-3.35 1.194-5.05.367-1.698-2.112-2.937-2.938-1.239-.643 1.286-1.286 2.617-1.929 3.903-.137-1.01-1.24-1.515-2.157-1.24.091-1.79-2.48-2.387-3.122-.55-.643 1.835-1.332 3.672-1.974 5.508.367-1.01.734-2.02 1.102-3.076.688-1.974-2.434-2.8-3.122-.872-.322.872-.643 1.745-.964 2.663a19.9 19.9 0 00-.092-1.653c-.138-1.745-2.663-2.25-3.168-.413-.643 2.433-1.653 4.637-2.8 6.75.367-2.159.734-4.363.826-6.567.092-1.882-2.617-2.11-3.168-.413-.505 1.653-1.056 3.306-1.56 5.004-.46 1.515-.78 3.168-1.745 4.408-.919 1.285.413 2.525 1.698 2.341zm4.316-1.423a1.81-1.81 0 00.23-.643c0-.413.23-.597.642-.643h.138c.23.23.55.138.826.138.643-.046 1.286 0 1.882-.046.505-.046 1.056 0 1.561-.138.23-.046.46-.046.69-.046 1.01 0 1.973-.183 2.983-.092 1.01.093 1.974 0 2.984 0 0-.045.046-.09.046-.09l-.183-.185c-.276-.23-.505-.46-.597-.826-.092-.368-.276-.735-.368-1.148-.045-.184 0-.413 0-.597 0-.46.184-1.01.276-1.47.046-.229.092-.458.23-.642.137-.184.413-.32.596-.23a1.01-1.01 0 01.23.138c.183.092.367.092.55.138.322.092.552.275.827.46.597.458 1.331.734 2.02 1.1.413.23.78.552 1.194.827.321.23.688.413 1.056.597.413.23.872.413 1.285.643.092.046.138.138.23.23l.321.32c.138.368.184.827.413 1.194.092.092.092.276.138.414.092.413.138.872.23 1.285 0 .092 0 .184-.046.276-.046.23-.092.46.091.642-.045.184-.09.322-.09.46a.503.503 0 01-.277.458l-.275.138c-.643.23-.918.872-1.378 1.286-.459.413-.826.872-1.423 1.056-.092 0-.137.092-.23.137-.183.092-.32.184-.504.322-.092.046-.23.092-.276.183-.32.368-.734.505-1.193.643-.23.092-.46.275-.69.413-.55.322-1.01.69-1.652.827-.046 0-.092.046-.138.092-.32.183-.55.092-.734-.23-.276-.459-.23-1.01-.367-1.469-.046-.184-.046-.367-.276-.46-.413-.275-.55-.78-.413-1.285.184-.596.55-1.056 1.01-1.469l.413-.413c-.688 0-1.33-.046-1.928.138a1.647 1.647 0 01-.505.092c-.413 0-.826.046-1.194.184-.046 0-.092 0-.137.045-.368.368-.919.414-1.378.597-.137.046-.23.138-.32.184-.552.046-1.057.092-1.608.184-.092 0-.184.137-.32.137-.23.046-.414.184-.644 0-.046-.046-.138 0-.184 0-.137 0-.32.046-.459.046-.642 0-1.24-.137-1.882-.23H7.79c-.413-.412-1.102-.55-1.194-1.285-.046-.32-.23-.642-.32-.964-.047-.137-.047-.275-.047-.413 0-.092.046-.184 0-.23-.092-.32.092-.55.184-.78z" /></svg>'
+		el[appendEl](arrow)
+
+		// changeCSS(el, style)
 		el.onclick = (e) => {
 			e.stopPropagation()
-			updateGallery(direction)
+			updateGallery(nextImage)
 		}
 		return el
 	}
 
-	// add style - if you want to tweak, run through beautifier
-	const style = document[createEl]('STYLE')
-	style.innerHTML =
-		'#bp_caption,#bp_container{bottom:0;left:0;right:0;position:fixed;opacity:0}#bp_container>*,#bp_loader{position:absolute;right:0;z-index:10}#bp_container,#bp_caption,#bp_container svg{pointer-events:none}#bp_container{top:0;z-index:9999;background:rgba(0,0,0,.7);opacity:0;transition:opacity .35s}#bp_loader{top:0;left:0;bottom:0;display:flex;align-items:center;cursor:wait;background:0;z-index:9}#bp_loader svg{width:50%;max-width:300px;max-height:50%;margin:auto;animation:bpturn 1s infinite linear}#bp_aud,#bp_container img,#bp_sv,#bp_vid{user-select:none;max-height:96%;max-width:96%;top:0;bottom:0;left:0;margin:auto;box-shadow:0 0 3em rgba(0,0,0,.4);z-index:-1}#bp_sv{background:#111}#bp_sv svg{width:66px}#bp_caption{font-size:.9em;padding:1.3em;background:rgba(15,15,15,.94);color:#fff;text-align:center;transition:opacity .3s}#bp_aud{width:650px;top:calc(50% - 20px);bottom:auto;box-shadow:none}#bp_count{left:0;right:auto;padding:14px;color:rgba(255,255,255,.7);font-size:22px;cursor:default}#bp_container button{position:absolute;border:0;outline:0;background:0;cursor:pointer;transition:all .1s}#bp_container>.bp-x{padding:0;height:41px;width:41px;border-radius:100%;top:8px;right:14px;opacity:.8;line-height:1}#bp_container>.bp-x:focus,#bp_container>.bp-x:hover{background:rgba(255,255,255,.2)}.bp-x svg,.bp-xc svg{height:21px;width:20px;fill:#fff;vertical-align:top;}.bp-xc svg{width:16px}#bp_container .bp-xc{left:2%;bottom:100%;padding:9px 20px 7px;background:#d04444;border-radius:2px 2px 0 0;opacity:.85}#bp_container .bp-xc:focus,#bp_container .bp-xc:hover{opacity:1}.bp-lr{top:50%;top:calc(50% - 130px);padding:99px 0;width:6%;background:0;border:0;opacity:.4;transition:opacity .1s}.bp-lr:focus,.bp-lr:hover{opacity:.8}@keyframes bpf{50%{transform:translatex(15px)}100%{transform:none}}@keyframes bpl{50%{transform:translatex(-15px)}100%{transform:none}}@keyframes bpfl{0%{opacity:0;transform:translatex(70px)}100%{opacity:1;transform:none}}@keyframes bpfr{0%{opacity:0;transform:translatex(-70px)}100%{opacity:1;transform:none}}@keyframes bpfol{0%{opacity:1;transform:none}100%{opacity:0;transform:translatex(-70px)}}@keyframes bpfor{0%{opacity:1;transform:none}100%{opacity:0;transform:translatex(70px)}}@keyframes bpturn{0%{transform:none}100%{transform:rotate(360deg)}}@media (max-width:600px){.bp-lr{font-size:15vw}}'
-	document.head[appendEl](style)
-
 	// create container element
 	container = document[createEl]('DIV')
-	container.id = 'bp_container'
+	container.className = 'bp-container'
 	container.onclick = close
-	closeButton = createCloseButton('bp-x')
-	container[appendEl](closeButton)
 	// gallery swipe listeners
 	if ('ontouchstart' in window) {
 		supportsTouch = true
@@ -249,43 +240,41 @@ function initialize() {
 
 	// create display video element
 	displayVideo = document[createEl]('VIDEO')
-	displayVideo.id = 'bp_vid'
+	displayVideo.className = 'bp-vid'
 	displayVideo.setAttribute('playsinline', true)
 	displayVideo.controls = true
 	displayVideo.loop = true
 
 	// create audio element
 	displayAudio = document[createEl]('audio')
-	displayAudio.id = 'bp_aud'
+	displayAudio.className = 'bp-aud'
 	displayAudio.controls = true
 	displayAudio.loop = true
 
-	// create gallery counter
-	galleryCounter = document[createEl]('span')
-	galleryCounter.id = 'bp_count'
-
 	// create caption elements
 	caption = document[createEl]('DIV')
-	caption.id = 'bp_caption'
-	captionHideButton = createCloseButton('bp-xc')
-	captionHideButton.onclick = toggleCaption.bind(null, false)
-	caption[appendEl](captionHideButton)
+	caption.className = 'bp-caption'
 	captionText = document[createEl]('SPAN')
+	// FV
+	captionBorder = document[createEl]('DIV')
+	captionBorder.classList.add('zigzag-border', 'w-full', 'left-0', 'right-0', 'absolute', 'z-10')
+	caption[appendEl](captionBorder)
+	// FV END
 	caption[appendEl](captionText)
 	container[appendEl](caption)
 
 	// left / right arrow icons
-	rightArrowBtn = createArrowSymbol(1, 'transform:scalex(-1)')
-	leftArrowBtn = createArrowSymbol(-1, 'left:0;right:auto')
+	leftArrowBtn = createArrowSymbol(-1, 'left')
+	rightArrowBtn = createArrowSymbol(1, 'right')
 
 	// create loading icon element
 	loadingIcon = document[createEl]('DIV')
-	loadingIcon.id = 'bp_loader'
+	loadingIcon.className = 'bp-loader'
 	loadingIcon.innerHTML =
 		'<svg viewbox="0 0 32 32" fill="#fff" opacity=".8"><path d="M16 0a16 16 0 0 0 0 32 16 16 0 0 0 0-32m0 4a12 12 0 0 1 0 24 12 12 0 0 1 0-24" fill="#000" opacity=".5"/><path d="M16 0a16 16 0 0 1 16 16h-4A12 12 0 0 0 16 4z"/></svg>'
 	// create youtube / vimeo container
 	iframeContainer = document[createEl]('DIV')
-	iframeContainer.id = 'bp_sv'
+	iframeContainer.className = 'bp-sv'
 
 	// create iframe to hold youtube / vimeo player
 	iframeSiteVid = document[createEl]('IFRAME')
@@ -333,7 +322,6 @@ function initialize() {
 		(e) => {
 			if (isOpen && !container.contains(e.target)) {
 				e.stopPropagation()
-				closeButton.focus()
 			}
 		},
 		true
@@ -400,8 +388,6 @@ function makeGallery(gallery, position) {
 	!~imgCache.indexOf(imgSrc) && toggleLoadingIcon(true)
 	if (galleryEls.length > 1) {
 		// if length is greater than one, add gallery stuff
-		container[appendEl](galleryCounter)
-		galleryCounter.innerHTML = `${galleryPosition + 1}/${galleryEls.length}`
 		if (!supportsTouch) {
 			// add arrows if device doesn't support touch
 			container[appendEl](rightArrowBtn)
@@ -447,10 +433,7 @@ function updateGallery(movement) {
 	}
 
 	// normalize position
-	galleryPosition = Math.max(
-		0,
-		Math.min(galleryPosition + movement, galleryLength)
-	)
+	galleryPosition = Math.max(0, Math.min(galleryPosition + movement, galleryLength))
 
 	// load images before and after for quicker scrolling through pictures
 	;[galleryPosition - 1, galleryPosition, galleryPosition + 1].forEach(
@@ -513,8 +496,6 @@ function changeGalleryImage(movement) {
 			el = activeEl.el
 		}
 	}
-	// update counter
-	galleryCounter.innerHTML = `${galleryPosition + 1}/${galleryEls.length}`
 	// show / hide caption
 	toggleCaption(galleryEls[galleryPosition].caption)
 	// execute onChangeImage callback
@@ -654,6 +635,7 @@ function open(err) {
 
 	// enlarge displayEl, fade in caption if hasCaption
 	setTimeout(() => {
+		displayElement.classList.add('rounded-lg', 'shadow-cover') // FV
 		displayElement.style.cssText += 'transition:transform .35s;transform:none'
 		captionContent && setTimeout(toggleCaption, 250, captionContent)
 	}, 60)
@@ -664,7 +646,6 @@ function close(e) {
 	const target = e ? e.target : container
 	const clickEls = [
 		caption,
-		captionHideButton,
 		displayVideo,
 		displayAudio,
 		captionText,
@@ -719,7 +700,6 @@ function removeContainer() {
 			container[removeEl](images[i])
 		}
 		isLoading && container[removeEl](loadingIcon)
-		container[removeEl](galleryCounter)
 		galleryOpen = galleryEls = false
 		preloadedImages = {}
 		supportsTouch || container[removeEl](rightArrowBtn)
